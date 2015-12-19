66cc4ca33d7d7a88c496c098e8976edfcb0ce7ec
Saguez LocalAdmin <sagmin@0909-016.local>
2015-12-19 13:27:28 +0100

    AUTOCAD 2016 for Mac - 1st generic commit

---
+++ ACAD2016/Install-ACAD2016-BusinessUnit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Thanks to :
+# download Install Autodesk AutoCAD 2016 for Mac.pkg to /tmp/Install Autodesk AutoCAD 2016 for Mac.pkg
+# http://forums.autodesk.com/t5/autocad-for-mac-general/deploying-autocad-2014-for-mac-to-computer-labs/td-p/5032058/page/2
+
+## Install AutoCAD 2016 for Mac - Saguez SUBSCRIPTION #11111111111111 n seat # 000-AABBCCDD 001H1
+
+/tmp/installACAD2016 "/tmp/Install Autodesk AutoCAD 2016 for Mac.pkg" S 000-AABBCCDD 001H1
