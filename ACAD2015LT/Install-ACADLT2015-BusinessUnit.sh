c24e1bba10ea578ada0798112063eeb7eb67bc04
Saguez LocalAdmin <sagmin@0909-016.local>
2015-12-19 13:19:06 +0100

    AUTOCAD LT 2015 for Mac - 1st generic commit

---
+++ ACAD2015LT/Install-ACADLT2015-BusinessUnit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Thanks to :
+# download Install Autodesk AutoCAD 2015 for Mac.pkg to /tmp/Install Autodesk AutoCAD 2015 for Mac.pkg
+# http://forums.autodesk.com/t5/autocad-for-mac-general/deploying-autocad-2014-for-mac-to-computer-labs/td-p/5032058/page/2
+
+## Install AutoCAD  LT 2015 for Mac - Company SUBSCRIPTION
+# 560-XXYYWWZZ / 827G1
+
+
+/tmp/installACADLT2015 "/tmp/Installer Autodesk AutoCAD LT 2015 for Mac.pkg" S 560-XXYYWWZZ 827G1 
