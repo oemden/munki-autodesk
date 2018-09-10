95111d0946855931c36d19868533f44d26ab2f5a
Saguez LocalAdmin <sagmin@0909-016.local>
2018-09-10 11:25:13 +0200

    AUTOCAD LT 2018 for Mac - 1st generic commit

---
+++ ACAD2018LT/Install-ACADLT2018-BusinessUnit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Thanks to :
+# download Install Autodesk AutoCAD 2016 for Mac.pkg to /tmp/Install Autodesk AutoCAD 2016 for Mac.pkg
+# http://forums.autodesk.com/t5/autocad-for-mac-general/deploying-autocad-2014-for-mac-to-computer-labs/td-p/5032058/page/2
+
+## Install AutoCAD  LT 2016 for Mac - Business Unit SUBSCRIPTION
+# 000-66778899 / 827J1
+# 827J1 for AutoCADLT 2018 for Mac.
+
+/tmp/installACADLT2018 "/tmp/Install Autodesk AutoCAD LT 2018 for Mac.pkg" S 000-66778899 827J1 
